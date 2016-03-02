d142ca66bbd4eb3fec595eed9e0ee4e0af9000a5
arcangelo <arcangelo.casavola@yoox.com>
2016-03-02 18:51:07 +0100

    c1

--- player.js
+++ player.js
@@ -3,27 +3,72 @@ exports = module.exports = {
 
   VERSION: 'Superstar poker player',
 
-  bet: function (gamestate, bet) {
+  myCard: null,
+  commonCards: null,
+  myId: null,
+  player: null,
+  betValue:Infinity,
 
-    //
+  getMyCard: function (gamestate) {
+    gamestate.players.forEach( function(v){
+      if(v.id == gamestate.me) {
+        this.myCard = v.cards;
+        this.player = v;
+      }
+    }.bind(this) );
+  },
+
+  getBet: function () {
+    if(this.isPair() && this.isPreFlop())
+      return Infinity;
+
+    if(this.isPair() )
+      return Infinity;
+
+    if(this.isThereIsFigure())
+      return Infinity;
+
+    return 0;
+  },
+  isPair: function(){
+    return (this.myCard[0].rank == this.myCard[1].rank)
+  },
+
+  isPreFlop: function(){
+    return (this.commonCards.length==0);
+  },
+
+  isThereIsFigure: function(){
+    if(isNaN(this.myCard[0].rank) || isNaN(this.myCard[1].rank))
+      return true;
+  },
+
+  bet: function (gamestate, bet) {
+    'use strict';
+
     // gamestate contains info about the state of the game.
     // check the documentation at https://bot-poker.herokuapp.com/wiki for further info about the data structure.
 
     //
     // bet is the function you should use to send your bet.
 
+    this.myId = gamestate.me;
 
-
+    this.getMyCard(gamestate);
     // enjoy the game!
 
+    this.commonCards = gamestate.commonCards;
+
+
+    this.betValue = this.getBet();
 
 
     //
     // currently we just go all-in every single hand!
 
-    'use strict';
 
-    return bet(Infinity);
+
+    return bet(betValue);
 
   }
 
